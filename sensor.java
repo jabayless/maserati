0d02193dc0682f134ff63fe1fe575ac29610ed72
mfout <56805367+mfout@users.noreply.github.com>
2019-12-05 20:21:01 +0000

    Add files via upload

--- sensor.java
+++ sensor.java
@@ -5,20 +5,20 @@ boolean sensorState, lastState;       // variables for reading the pushbutton st
 
 public static void main(String args[]) throws InterruptedException, IOException {
   // initialize the sensor pin as an input:
-  GpioController gpio = GpioFactory.getInstance();
-  GpioPinDigitalInput sensorPin = gpio.provisionDigitalInputPin(RaspiPin.GPIO_12, "sensor", PinPullResistance.PULL_DOWN);
-  boolean inputVal = inputPin.isHigh();
+  final GpioController gpio = GpioFactory.getInstance();	//create gpio controller to communicate with pins
+  GpioPinDigitalInput sensorPin = gpio.provisionDigitalInputPin(RaspiPin.GPIO_12, "sensor", PinPullResistance.PULL_DOWN);	//mapping the sensor pin
+  boolean inputVal = sensorPin.isHigh();
   
-  final Console console = new Console();
-  console.promtForExit();
+  final Console console = new Console();	//creates console object to communicate with user
+  console.promtForExit();	//allows user to exit with CTRL+C
 
   final Serial serial = SerialFactory.createInstance();
   serial.addListener(new SerialDataEventListener() {
 	  @Override
-	  public void dataReceived(SerialDataEvent event) {
+	  public void dataReceived(SerialDataEvent event) {	//tests if data is working
 		try {
-			console.println("[HEX DATA] " + event.getHexByteString());
-			console.println("[ASCII DATA]" + event.getAsciiString());
+			console.println("[HEX DATA]: " + event.getHexByteString());
+			console.println("[ASCII DATA]: " + event.getAsciiString());
 		}  
 		catch (IOException e) {
 			e.printStackTrace();
@@ -32,7 +32,7 @@ public static void main(String args[]) throws InterruptedException, IOException 
 		e.printStackTrace();
 	}
 	
-	config.device(SerialPort.getDefaultPort())
+	config.device(SerialPort.getDefaultPort())	//attributes of the port
 		.baud(Baud._38400)
 		.dataBits(DataBits._8)
 		.parity(Parity.NONE)
@@ -43,16 +43,17 @@ public static void main(String args[]) throws InterruptedException, IOException 
 		config = CommandArgumentParser.getSerialConfig(config, args);
 	}
 	
-	serial.open(config);
+	serial.open(config);	//starts serial communication
 	
+	//loop to detect state
 	while(console.isRunning()) {
 		try {
-			SensorState = sensorPin.isHigh();
+			sensorState = sensorPin.isHigh();
 			if (sensorState && !lastState) {
-				System.out.println("Working")
+				serial.println("Working")
 			}
 			if (!sensorState && lastState) {
-				System.out.println("Broken");
+				serial.println("Broken");
 			}
 			lastState = sensorState;
 		}
